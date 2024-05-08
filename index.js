9132ff3e612fb3257028a6106fab053a03dabbfb
nickadams1998 <161159305+nickadams1998@users.noreply.github.com>
2024-05-07 17:59:52 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-//jshint esversion:6
-// Comment in the next line to disable the warning about the global variable being used
-// create an express object from the express package
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// create an app object from the express object
+
 const app = express();
-// this allows the parsing of the html file using body parser
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-//this sends the html file to the web page using the root directory
-//
-app.get("/VolCalc", function(req, res) {
-  res.sendFile(__dirname + "/VolCalculator.html")
+
+app.set('view engine', 'ejs');
+
+
+app.get("/calculate-height", (req, res) => {
+  res.render("VolCalculator");
 });
 
-// this gets the response from the values in the web page
-app.post("/", function(req, res){
+app.post("/calculate-height", (req, res) => {
+  const radius = Number(req.body.radius);
+  const volume = Number(req.body.volume);
 
-//use n1 for radium and n2 for height
-  var num1 = Number(req.body.n1);
-  var num2 = Number(req.body.n2);
 
-// does the computation of the input variables, as numbers
-//This is a new comment that is gor GitHub
-  var volCylinder = Math.PI * Math.pow(num1, 2) * num2;
+  const height = (3 * volume) / (Math.PI * Math.pow(radius, 2));
 
-// sends the results back to the web page as string
-  res.send("The volume of the cylinder is " + volCylinder.toFixed(2));
-})
-//this gets the response from the web page to this placeholder
+  res.send(`The height of the cone is ${height.toFixed(2)} units.`);
+});
 
-app.listen(3000, function() {
-  console.log ("Server is running on port 3000")
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
